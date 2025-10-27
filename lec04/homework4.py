b0130d2babb9e0ffc7adc0a54968cfbbf46c67cd
DeshanNX <149660175+DeshanNX@users.noreply.github.com>
2025-10-27 23:46:44 +0900

    Update homework4.py

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -12,7 +12,14 @@ def next_birthday(date, birthdays):
     birthday - the next day, after given date, on which somebody has a birthday
     list_of_names - list of all people with birthdays on that date
     '''
-    birthday = (1,1)
-    list_of_names = []
-    return birthday, list_of_names
-    
+    # Convert all keys (month, day) to a sorted list
+    sorted_dates = sorted(birthdays.keys())
+
+    # Loop through sorted dates and find the first one that comes after the given date
+    for bday in sorted_dates:
+        if bday > date:     # tuple comparison works automatically in Python
+            return bday, birthdays[bday]
+
+    # If no later date is found, wrap around to the first in the year
+    first_date = sorted_dates[0]
+    return first_date, birthdays[first_date]
